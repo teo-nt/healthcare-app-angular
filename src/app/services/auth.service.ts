6111ae5f7476307bdd5c63b7cd33a8734c7c4ec0
Teo <thod_ntonas@hotmail.com>
2024-06-16 00:55:18 +0300

    Implement utility methods and add script of bootstrap

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,11 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { environment } from 'src/environments/environment.development';
 import { LoginRequest } from '../interfaces/login-request';
 import { HttpClient } from '@angular/common/http';
 import { LoginResponse } from '../interfaces/login-response';
+import { jwtDecode } from 'jwt-decode';
+import { LoggedInUser } from '../interfaces/logged-in-user';
+import { Router } from '@angular/router';
 
 const API_URL = environment.apiUrl;
 
@@ -11,9 +14,39 @@ const API_URL = environment.apiUrl;
 })
 export class AuthService {
   http = inject(HttpClient)
-  constructor() { }
+  user = signal<LoggedInUser | null>(null)
+  router = inject(Router)
 
   login(data: LoginRequest) {
     return this.http.post<LoginResponse>(`${API_URL}/user/login`, data)
+  }
+
+  logout() {
+    localStorage.removeItem('auth_token')
+    this.user.set(null)
+    this.router.navigate(['welcome', 'login'])
+  }
+
+  getToken() {
+    return localStorage.getItem('auth_token') || ''
+  }
+
+  isLoggedIn() {
+    const token = this.getToken()
+    if (!token) return false;
+
+    if (this.isJwtExpired()){
+      this.logout()
+      return false
+    }
+    return true
+  }
+
+  isJwtExpired() {
+    const token = this.getToken()
+    if (!token) return true;
+
+    const decodedToken = jwtDecode(token)
+    return Date.now() >= decodedToken.exp * 1000
   }
 }
