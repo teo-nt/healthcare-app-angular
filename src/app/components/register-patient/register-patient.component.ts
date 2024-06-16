d8f97eac152b9249e1344304e02021e049e4a7ad
Teo <thod_ntonas@hotmail.com>
2024-06-16 03:37:04 +0300

    Finish the patient register form

--- src/app/components/register-patient/register-patient.component.ts
+++ src/app/components/register-patient/register-patient.component.ts
@@ -4,11 +4,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { MatRadioModule } from '@angular/material/radio'
 
 @Component({
   selector: 'app-register-patient',
   standalone: true,
-  imports: [MatInputModule, MatIconModule, ReactiveFormsModule, RouterLink],
+  imports: [MatInputModule, MatIconModule, MatRadioModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register-patient.component.html',
   styleUrl: './register-patient.component.css'
 })
@@ -18,13 +19,13 @@ export class RegisterPatientComponent {
   router = inject(Router)
   
   form = new FormGroup({
-    username: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(30)]),
+    username: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(30), Validators.pattern('^\\S{5,}$')]),
     password: new FormControl('', [Validators.required, Validators.pattern('(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*\\W)^.{8,}$')]),
     confirmPassword: new FormControl(''),
     email: new FormControl('', [Validators.email, Validators.required]),
-    firstname: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
-    lastname: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
-    phoneNumber: new FormControl('', Validators.required),
+    firstname: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^\\S{3,}$')]),
+    lastname: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50), Validators.pattern('^\\S{3,}$')]),
+    phoneNumber: new FormControl('', [Validators.required, Validators.pattern('^\\d{10,14}$')]),
     age: new FormControl(18, [Validators.max(100), Validators.min(18), Validators.required]),
     gender: new FormControl(),
     medicalHistory: new FormControl()
@@ -39,6 +40,6 @@ export class RegisterPatientComponent {
   }
 
   registerPatient() {
-
+    console.log(this.form.value)
   }
 }
