b7f3dcf58b6f9d2cac85bcf6a50791ad40e322c6
Teo <thod_ntonas@hotmail.com>
2024-06-17 04:33:18 +0300

    Create interfaces for register and error response on registration

---
+++ src/app/interfaces/register-patient.ts
@@ -0,0 +1,12 @@
+export interface RegisterPatient {
+    username: string;
+    password: string;
+    confirmPassword: string;
+    email: string;
+    firstname: string;
+    lastname: string;
+    phoneNumber: string;
+    age: number;
+    gender: string;
+    medicalHistory: string;
+}
