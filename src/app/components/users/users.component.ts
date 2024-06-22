19acab434de7d2dc249b8cf5824a5c0bd78e19d3
Teo <thod_ntonas@hotmail.com>
2024-06-23 01:11:00 +0300

    Add sorting of users

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,57 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { sortBy } from 'lodash-es';
+import { UserDetails } from 'src/app/interfaces/user-details';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
   authService = inject(AuthService)
-  users = this.authService.getAllUsers()
+  users: UserDetails[] = []
+
+  ngOnInit(): void {
+    this.authService.getAllUsers().subscribe((data: UserDetails[]) => {
+      this.users = data
+    })
+  }
+
+  sortOrder = {
+    username: 'none',
+    email: 'none',
+    role: 'none',
+    status: 'none'
+  }
+   
+  sortUsers(sortKey: string) {
+    if (this.sortOrder[sortKey] === 'asc') {
+      this.sortOrder[sortKey] = 'desc'
+      this.users = sortBy(this.users, sortKey).reverse()
+    } else {
+      this.sortOrder[sortKey] = 'asc'
+      this.users = sortBy(this.users, sortKey)
+    }
+
+    for (let key in this.sortOrder) {
+      if (key !== sortKey) {
+        this.sortOrder[key] = 'none'
+      }
+    }
+  }
+
+  sortSign(sortKey: string) {
+    if (this.sortOrder[sortKey] === 'asc') {
+      return '↑';
+    } else if (this.sortOrder[sortKey] === 'desc') {
+      return '↓';
+    } else {
+      return '';
+    }
+  }
+  
 }
