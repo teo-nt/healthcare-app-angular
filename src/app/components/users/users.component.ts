3cb9bad1b2b32ea5235d2eb6976a7bdff672ed6f
Teo <thod_ntonas@hotmail.com>
2024-06-23 08:51:51 +0300

    Add functionality of showing account details when admin clicks a row

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,5 +1,6 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 import { sortBy } from 'lodash-es';
 import { UserDetails } from 'src/app/interfaces/user-details';
 import { AuthService } from 'src/app/services/auth.service';
@@ -7,17 +8,24 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, FormsModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent implements OnInit {
   authService = inject(AuthService)
   users: UserDetails[] = []
+  tempUsers: UserDetails[] = []
+  selectedUserId: number | null = null
+  selectedAll = true
+  selectedDoctors = false
+  selectedPatients = false
+  inputUsername = ''
 
   ngOnInit(): void {
     this.authService.getAllUsers().subscribe((data: UserDetails[]) => {
       this.users = data
+      this.tempUsers = this.users
     })
   }
 
@@ -31,10 +39,10 @@ export class UsersComponent implements OnInit {
   sortUsers(sortKey: string) {
     if (this.sortOrder[sortKey] === 'asc') {
       this.sortOrder[sortKey] = 'desc'
-      this.users = sortBy(this.users, sortKey).reverse()
+      this.tempUsers = sortBy(this.tempUsers, sortKey).reverse()
     } else {
       this.sortOrder[sortKey] = 'asc'
-      this.users = sortBy(this.users, sortKey)
+      this.tempUsers = sortBy(this.tempUsers, sortKey)
     }
 
     for (let key in this.sortOrder) {
@@ -46,12 +54,47 @@ export class UsersComponent implements OnInit {
 
   sortSign(sortKey: string) {
     if (this.sortOrder[sortKey] === 'asc') {
-      return '↑';
+      return '▲';
     } else if (this.sortOrder[sortKey] === 'desc') {
-      return '↓';
+      return '▼';
     } else {
       return '';
     }
   }
-  
+
+  toggleDetails(id: number) {
+    this.selectedUserId = this.selectedUserId === id ? null : id
+  }
+
+  showAll() {
+    this.tempUsers = this.users
+    this.selectedAll = true
+    this.selectedDoctors = false
+    this.selectedPatients = false
+    this.selectedUserId = null
+  }
+
+  showDoctors() {
+    this.tempUsers = this.users.filter(u => u.userRole === 'Doctor')
+    this.selectedDoctors = true
+    this.selectedAll = false
+    this.selectedPatients = false
+    this.selectedUserId = null
+  }
+
+  showPatients() {
+    this.tempUsers = this.users.filter(u => u.userRole === 'Patient')
+    this.selectedPatients = true
+    this.selectedAll = false
+    this.selectedDoctors = false
+    this.selectedUserId = null
+  }
+
+  searchByUsername() {
+    this.tempUsers = this.users.filter(u => u.username.includes(this.inputUsername))
+    this.selectedAll = true
+    this.selectedDoctors = false
+    this.selectedPatients = false
+    this.selectedUserId = null
+  }
 }
