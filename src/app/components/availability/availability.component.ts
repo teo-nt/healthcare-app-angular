d4c22d4a304ea96df3e62f424a8025369e3fe915
Teo <thod_ntonas@hotmail.com>
2024-06-27 04:32:44 +0300

    Allow doctor to see all timeslots for selected date

--- src/app/components/availability/availability.component.ts
+++ src/app/components/availability/availability.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { MonthAvailabilityComponent } from '../month-availability/month-availability.component';
+import { DoctorService } from 'src/app/services/doctor.service';
+import { Timeslot } from 'src/app/interfaces/timeslot';
 
 @Component({
   selector: 'app-availability',
   standalone: true,
-  imports: [],
+  imports: [MonthAvailabilityComponent],
   templateUrl: './availability.component.html',
   styleUrl: './availability.component.css'
 })
-export class AvailabilityComponent {
+export class AvailabilityComponent implements OnInit {
+  doctorService = inject(DoctorService)
+  monthIndexes = [0, 1, 2]  // 0: currentMonth, 1: next month...
+  timeslots: Timeslot[]
+  shownTimeslots: Timeslot[] = []
 
+  ngOnInit(): void {
+    this.doctorService.getTimeSlots().subscribe({
+      next: (response) => {
+        this.timeslots = response
+        console.log(this.timeslots)
+      }
+    })
+  }
+   
+  showTimeslots(date: Date) {
+    this.shownTimeslots = []
+    for (const slot of this.timeslots) {
+      let localDate = new Date(`${slot.date}T00:00:00`)
+      if (localDate.getTime() === date.getTime()) {
+        this.shownTimeslots.push(slot)
+      }
+    }
+  }
 }
---
+++ src/app/components/month-availability/month-availability.component.ts
@@ -0,0 +1,84 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-month-availability',
+  standalone: true,
+  imports: [],
+  templateUrl: './month-availability.component.html',
+  styleUrl: './month-availability.component.css'
+})
+export class MonthAvailabilityComponent implements OnInit {
+  @Input() index: number;
+  @Output() onClickingDate: EventEmitter<Date> = new EventEmitter<Date>();
+  monthTable = [[]]
+  currentMonth: string;
+  currentMonthNumber: number;
+  currentYear: number;
+
+  monthNames = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ]
+
+  ngOnInit() {
+    this.createMonthTable()
+    this.getMonthName()
+    this.currentMonthNumber = this.monthNames.indexOf(this.currentMonth)
+    this.getYear()
+  }
+
+  /**
+   * Get a month from current date by index. 
+   * If index = 0, then current month is returned,
+   * if index = 1, then next month is returned etc.
+   *
+   * @param index The index indicating the month from current month.
+   * @returns The month from current month according to index.
+   */
+  getMonthName() {
+    this.currentMonth = this.monthNames[(new Date().getMonth() + this.index) % 12]
+  }
+
+  getYear() {
+    const currentDate = new Date();
+    currentDate.setMonth(currentDate.getMonth() + this.index)
+    this.currentYear = currentDate.getFullYear()
+  }
+
+  getDaysOfMonth() {
+    let now = new Date();
+    
+    // Get the current year and month
+    let year = now.getFullYear();
+    let month = now.getMonth();
+
+    let nextMonth = new Date(year, month + 1 + this.index, 1);
+    let currentMonth = nextMonth.setDate(0)
+    return new Date(currentMonth).getDate()
+  }
+
+  createMonthTable() {
+    const days = this.getDaysOfMonth()
+
+    for (let i = 1; i <= days; i++) {
+      this.monthTable[this.monthTable.length - 1].push(i)
+      if (i % 7 === 0) {
+        this.monthTable.push([])
+      }
+    }
+  }
+
+  onShowingDayTimeslots(day) {
+    if (this.isPast(day)) return
+    const date = new Date(this.currentYear, this.currentMonthNumber, day)
+    this.onClickingDate.emit(date)
+  }
+
+  isPast(day: number) {
+    const checkedDate = new Date(this.currentYear, this.currentMonthNumber, day, 22)
+    if (checkedDate < new Date()) {
+      return true;
+    }
+    return false
+  }
+}
